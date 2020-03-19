6760b79158946f4f9af75bbbda2229f19863d442
Luca <lm28073@pausd.us>
2020-03-19 10:47:29 -0700

    added the Trie

--- src/Trie.java
+++ src/Trie.java
@@ -1,2 +1,74 @@
-public class Trie {
+public class Trie<Value> {
+
+    private final static int RADIX = 256;
+    private Node root = new Node();
+
+    private static class Node {
+        private Object val;
+        private Node[] next = new Node[RADIX];
+    }
+
+    public Value get(String key) {
+        Node x = get(root, key, 0);
+
+        if (x == null) {
+            return null;
+        }
+        return (Value) x.val;
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return x;
+        }
+        char c = key.charAt(d);
+        return get(x.next[c], key, d + 1);
+    }
+
+    public void put(String key, Value val) {
+        root = put(root, key, val, 0);
+    }
+
+    private Node put(Node x, String key, Value val, int d) {
+        if (x == null) {
+            x = new Node();
+        }
+        if (d == key.length()) {
+            x.val = val;
+            return x;
+        }
+        char c= key.charAt(d);
+        x.next[c] = put(x.next[c], key, val, d + 1);
+        return x;
+    }
+
+    // TODO: implement prefix stuff
+
+    public void delete(String key) {
+        root = delete(root, key, 0);
+    }
+
+    private Node delete(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            x.val = null;
+        } else {
+            char c = key.charAt(d);
+            x.next[c] = delete(x.next[c], key, d + 1);
+        }
+        if (x.val != null) {
+            return x;
+        }
+        for (char c = 0; c < RADIX; c++) {
+            if (x.next[c] != null) {
+                return x;
+            }
+        }
+        return null;
+    }
 }
