6092457d624a3146673be451bcd3de8b0dc8c9af
Luca <lm28073@pausd.us>
2020-03-19 09:33:48 -0700

    created the generation of a suffix array

--- src/SuffixArray.java
+++ src/SuffixArray.java
@@ -2,71 +2,57 @@ import java.util.Arrays;
 
 public class SuffixArray {
 
-    int[] array;
+    private Suffix[] mSuffixes;
 
-    public SuffixArray(String string) {
-        int length = string.length();
-        Suffix[] suffixes = new Suffix[length];
-
-        for (int i = 0; i < length; i++) {
-            suffixes[i] = new Suffix(i, string.charAt(i) - '$', 0);
+    public SuffixArray(String text) {
+        int n = text.length();
+        mSuffixes = new Suffix[n];
+        for (int i = 0; i < n; i++) {
+            mSuffixes[i] = new Suffix(text, i);
         }
-        for (int i = 0; i < length; i++)
-            suffixes[i].next = (i + 1 < length ? suffixes[i + 1].rank : -1);
-
-        Arrays.sort(suffixes);
-
-        int[] ind = new int[length];
-
-
-        for (int suffixLength = 4; suffixLength < 2 * length; suffixLength <<= 1) {
-
-            int rank = 0, prev = suffixes[0].rank;
-            suffixes[0].rank = rank;
-            ind[suffixes[0].index] = 0;
-            for (int i = 1; i < length; i++) {
-                if (suffixes[i].rank == prev &&
-                        suffixes[i].next == suffixes[i - 1].next) {
-                    prev = suffixes[i].rank;
-                    suffixes[i].rank = rank;
-                } else {
-                    prev = suffixes[i].rank;
-                    suffixes[i].rank = ++rank;
-                }
-                ind[suffixes[i].index] = i;
-            }
-
-            for (int i = 0; i < length; i++) {
-                int nextP = suffixes[i].index + suffixLength / 2;
-                suffixes[i].next = nextP < length ?
-                        suffixes[ind[nextP]].rank : -1;
-            }
-
-            Arrays.sort(suffixes);
-        }
-
-        int[] suf = new int[length];
-
-        for (int i = 0; i < length; i++)
-            suf[i] = suffixes[i].index;
-
-        array = suf;
+        Arrays.sort(mSuffixes);
     }
 
-    public static class Suffix implements Comparable<Suffix> {
-        int index;
-        int rank;
-        int next;
+    private static class Suffix implements Comparable<Suffix> {
 
-        public Suffix(int ind, int r, int nr) {
-            index = ind;
-            rank = r;
-            next = nr;
+        private String mText;
+        private int mIndex;
+
+        private Suffix(String text, int index) {
+            mText = text;
+            mIndex = index;
         }
 
-        public int compareTo(Suffix s) {
-            if (rank != s.rank) return Integer.compare(rank, s.rank);
-            return Integer.compare(next, s.next);
+        private int length() {
+            return mText.length() - mIndex;
         }
+
+        private char charAt(int i) {
+            return mText.charAt(mIndex + i);
+        }
+
+        public int compareTo(Suffix o) {
+            if (this == o) return 0;
+            int n = Math.min(this.length(), o.length());
+            for (int i = 0; i < n; i++) {
+                if (this.charAt(i) < o.charAt(i)) return -1;
+                if (this.charAt(i) > o.charAt(i)) return +1;
+            }
+            return this.length() - o.length();
+        }
+
+        public String toString() {
+            return mText.substring(mIndex);
+        }
+    }
+
+    public int length() {
+        return mSuffixes.length;
+    }
+
+
+    public int index(int i) {
+        if (i < 0 || i >= mSuffixes.length) throw new IllegalArgumentException();
+        return mSuffixes[i].mIndex;
     }
 }
