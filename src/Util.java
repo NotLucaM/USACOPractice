20badfeef7a3850f776ac9b799296babc0329995
Luca <lm28073@pausd.us>
2020-03-19 15:57:46 -0700

    added KMPSearch

--- src/Util.java
+++ src/Util.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class Util {
 
@@ -29,11 +32,66 @@ public class Util {
         return -1;
     }
 
+    public static ArrayList<Integer> KMPSearch(String text, String substr) {
+        int M = substr.length();
+        int N = text.length();
+
+        ArrayList<Integer> matches = new ArrayList<>();
+        int[] lps = new int[M];
+        int j = 0;
+
+        computeLPSArray(substr, M, lps);
+
+        int i = 0;
+        while (i < N) {
+            if (substr.charAt(j) == text.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                matches.add(i - j);
+                j = lps[j - 1];
+            } else if (i < N && substr.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i = i + 1;
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    private static void computeLPSArray(String pat, int M, int[] lps) {
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+
+
+                } else {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+
     @Test
     public void binarySearch() {
-        int[] arr = { 2, 3, 4, 10, 40 };
-        int[] empty = { };
-        int[] oneVal = { 2 };
+        int[] arr = {2, 3, 4, 10, 40};
+        int[] empty = {};
+        int[] oneVal = {2};
 
         assertEquals(3, binarySearch(arr, 10));
         assertEquals(-1, binarySearch(arr, 24));
@@ -42,5 +100,15 @@ public class Util {
         assertEquals(0, binarySearch(arr, 2));
         assertEquals(-1, binarySearch(empty, 23));
         assertEquals(0, binarySearch(arr, 2));
+    }
+
+    @Test
+    public void testKMPSearch() {
+        String text = "ABABDABACDABABCABAB";
+        String longPattern = "ABABCABAB";
+        String shortPattern = "AB";
+
+        assertEquals(new Integer(10), KMPSearch(text, longPattern).get(0));
+        assertEquals(new Integer(2), KMPSearch(text, shortPattern).get(1));
     }
 }
