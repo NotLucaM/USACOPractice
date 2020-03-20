2aef6b56ef8b8e576bc013155fdcb0290a00c619
Luca <lm28073@pausd.us>
2020-03-20 16:49:11 -0700

    added topological sort

--- src/Graph.java
+++ src/Graph.java
@@ -7,20 +7,32 @@ public class Graph {
     private ArrayList<Node>[] graph;
     private int vertices;
     private int edges;
+    private boolean bidirectional;
 
     public Graph(int vertices) {
+        this(vertices, true);
+    }
+
+    public Graph(int vertices, boolean bidirectional) {
         this.graph = new ArrayList[vertices];
         this.vertices = vertices;
         this.edges = 0;
+        this.bidirectional = bidirectional;
 
         for (int i = 0; i < vertices; i++) {
             graph[i] = new ArrayList<>();
         }
     }
 
+    public void addEdge(int source, int destination) {
+        addEdge(source, destination, 1);
+    }
+
     public void addEdge(int source, int destination, int cost) {
         graph[source].add(new Node(destination, cost));
-        graph[destination].add(new Node(source, cost));
+        if (bidirectional) {
+            graph[destination].add(new Node(source, cost));
+        }
         edges++;
     }
 
@@ -68,9 +80,61 @@ public class Graph {
         return distances;
     }
 
-    private static class Node implements Comparable<Node> {
-        private int destination;
-        private int cost;
+    public void floydWarshall() { // TODO, implement floydWarshall
+        throw new NotImplementedException();
+//        for (int k = 0; k < vertices; k++) {
+//            for (int i = 0; i < vertices; i++) {
+//                for (int j = 0; j < vertices; j++) {
+//                    if (graph[i].get(k).cost + graph[k].get(j).cost < graph[i].get(j).cost) {
+//                        graph[i].get(j).cost = graph[i].get(k).cost + graph[k].get(j).cost;
+//                    }
+//                }
+//            }
+//        }
+    }
+
+    private void topologicalSort(int v, boolean[] visited, Stack<Integer> stack) {
+        visited[v] = true;
+        Node i;
+
+        for (Node node : graph[v]) {
+            i = node;
+            if (!visited[i.destination]) {
+                topologicalSort(i.destination, visited, stack);
+            }
+        }
+
+        stack.push(v);
+    }
+
+    public int[] topologicalSort() {
+        Stack<Integer> stack = new Stack<>();
+
+        boolean[] visited = new boolean[vertices];
+        for (int i = 0; i < vertices; i++) {
+            visited[i] = false;
+        }
+
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i]) {
+                topologicalSort(i, visited, stack);
+            }
+        }
+
+        int[] sorted = new int[vertices];
+        for (int i = 0; i < vertices; i++) {
+            sorted[i] = stack.pop();
+        }
+        return sorted;
+    }
+
+    public ArrayList<Node>[] getGraph() {
+        return graph;
+    }
+
+    public static class Node implements Comparable<Node> {
+        int destination;
+        int cost;
 
         public Node(int destination, int cost) {
             this.destination = destination;
--- src/GraphTest.java
+++ src/GraphTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 class GraphTest {
@@ -13,5 +15,39 @@ class GraphTest {
         graph.addEdge(3, 2, 12);
 
         assertArrayEquals(new int[]{0, 24, 3, 15}, graph.dijkstra(0));
+    }
+
+    @Test
+    void floydWarshall() {
+        return;
+
+//        Graph graph = new Graph(4);
+//        graph.addEdge(0, 1, 5);
+//        graph.addEdge(0, 3, 10);
+//        graph.addEdge(1, 2, 3);
+//
+//        graph.floydWarshall();
+//
+//        ArrayList<Graph.Node>[] edges = graph.getGraph();
+//        int i = 0;
+//        for (ArrayList<Graph.Node> node : edges) {
+//            for (Graph.Node edge : node) {
+//                System.out.println("Source: " + i + " Destination: " + edge.destination + " Cost: " + edge.cost);
+//            }
+//            i++;
+//        }
+    }
+
+    @Test
+    void topologicalSort() {
+        Graph g = new Graph(6, false);
+        g.addEdge(5, 2);
+        g.addEdge(5, 0);
+        g.addEdge(4, 0);
+        g.addEdge(4, 1);
+        g.addEdge(2, 3);
+        g.addEdge(3, 1);
+
+        assertArrayEquals(new int[]{5, 4, 2, 3, 1, 0}, g.topologicalSort());
     }
 }
