37c228646dd25f29e39bfcf4a7c1c0a3b1c383e9
Luca <lm28073@pausd.us>
2020-03-23 15:52:15 -0700

    added input reading for talent

--- src/practice/talent.java
+++ src/practice/talent.java
@@ -3,6 +3,17 @@
  problem http://www.usaco.org/index.php?page=viewproblem2&cpid=839
  */
 
+import java.io.*;
+import java.util.StringTokenizer;
+
+@SuppressWarnings("ALL")
 public class talent {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("milkorder.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("milkorder.out")));
+        StringTokenizer st;
+
+
+    }
 }
