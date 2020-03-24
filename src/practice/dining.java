fdf40a0200a89c152a9b80248bd5ab51fdd6aa77
Luca <lm28073@pausd.us>
2020-03-24 15:19:19 -0700

    added input for the problem dining

---
+++ src/practice/dining.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class dining {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("talent.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("talent.out")));
+        StringTokenizer st;
+    }
+}
