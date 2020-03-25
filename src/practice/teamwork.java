67b6d1c43bdf4d141da06410f9fa0feb2ed02070
Luca <lm28073@pausd.us>
2020-03-25 12:32:47 -0700

    added input reading for teamwork

--- src/practice/teamwork.java
+++ src/practice/teamwork.java
@@ -1,7 +1,10 @@
 package practice;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
 
 public class teamwork {
 
@@ -9,5 +12,14 @@ public class teamwork {
         BufferedReader in = new BufferedReader(new FileReader("teamwork.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("teamwork.out")));
         StringTokenizer st;
+
+        st = new StringTokenizer(in.readLine());
+        int cows = parseInt(st.nextToken());
+        int teamSize = parseInt(st.nextToken());
+
+        int[] cowSkills = new int[cows];
+        for (int i = 0; i < cows; i++) {
+            cowSkills[i] = parseInt(in.readLine());
+        }
     }
 }
