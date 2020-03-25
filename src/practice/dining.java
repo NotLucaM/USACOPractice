9563fcf03aeb474b34115417e14a2b8da4727f38
Luca <lm28073@pausd.us>
2020-03-24 17:51:10 -0700

    finished reading input and copied algorithms over from other classes

--- src/practice/dining.java
+++ src/practice/dining.java
@@ -1,13 +1,102 @@
 package practice;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.min;
+import static java.lang.Math.max;
 
 public class dining {
 
+    private static int cowNum;
+    private static int edgeNum;
+    private static int foodNum;
+    private static ArrayList<Node>[] graph;
+    private static ArrayList<Node> food;
+
+    public int[] dijkstra(int source) {
+        int[] distances = new int[cowNum];
+        Set<Integer> settled = new HashSet<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        pq.add(new Node(source, 0));
+        distances[source] = 0;
+
+        while (settled.size() != cowNum) {
+            if (pq.isEmpty()) {
+                return distances;
+            }
+
+            int u = pq.remove().destination;
+            settled.add(u);
+
+            for (int i = 0; i < graph[u].size(); i++) {
+                Node v = graph[u].get(i);
+
+                if (!settled.contains(v.destination)) {
+                    int edgeDistance = v.cost;
+                    int newDistance = distances[u] + edgeDistance;
+
+                    if (newDistance < distances[v.destination]) {
+                        distances[v.destination] = newDistance;
+                    }
+
+                    pq.add(new Node(v.destination, distances[v.destination]));
+                }
+            }
+        }
+
+        return distances;
+    }
+
+
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader("talent.in"));
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("talent.out")));
+        BufferedReader in = new BufferedReader(new FileReader("dining.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("dining.out")));
         StringTokenizer st;
+
+        st = new StringTokenizer(in.readLine());
+        cowNum = parseInt(st.nextToken());
+        edgeNum = parseInt(st.nextToken());
+        foodNum = parseInt(st.nextToken());
+
+        graph = new ArrayList[cowNum];
+        food = new ArrayList<>();
+
+        for (int i = 0; i < edgeNum; i++) {
+            st = new StringTokenizer(in.readLine());
+            int from = parseInt(st.nextToken()) - 1;
+            int to = parseInt(st.nextToken()) - 1;
+            int cost = parseInt(st.nextToken()) - 1;
+
+            graph[from].add(new Node(to, cost));
+        }
+
+        for (int i = 0; i < foodNum; i++) {
+            st = new StringTokenizer(in.readLine());
+            int location = parseInt(st.nextToken()) - 1;
+            int cost = parseInt(st.nextToken()) - 1;
+
+            food.add(new Node(location, cost));
+        }
+
+
+    }
+
+    public static class Node implements Comparable<Node> {
+        int destination;
+        int cost;
+
+        public Node(int destination, int cost) {
+            this.destination = destination;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return this.cost - that.cost;
+        }
     }
 }
