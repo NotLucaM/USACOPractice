92797d202dc910472c7cd57ef0eb65d2aebc0bb2
Luca <lm28073@pausd.us>
2020-03-25 13:08:42 -0700

    made class snowboots for its respective problem

---
+++ src/practice/snowboots.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+@SuppressWarnings("ALL")
+public class snowboots {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("teamwork.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("teamwork.out")));
+        StringTokenizer st;
+    }
+}
