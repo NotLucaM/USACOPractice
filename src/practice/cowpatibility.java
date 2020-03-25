4ff4b44abcca2bbc5c7eea650391d79a0ad5d170
Luca <lm28073@pausd.us>
2020-03-25 11:51:20 -0700

    created class cowpatibility

---
+++ src/practice/cowpatibility.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class cowpatibility {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("cowpatibility.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowpatibility.out")));
+        StringTokenizer st;
+    }
+}
