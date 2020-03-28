dbf4383a35afb3a85a85891c7dcdc50b34e43842
Luca <lm28073@pausd.us>
2020-03-28 16:03:24 -0700

    more problems done?

--- src/practice/art2.java
+++ src/practice/art2.java
@@ -1,21 +1,19 @@
-package practice;
-
 import java.io.*;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
+import static java.lang.Integer.max;
 import static java.lang.Integer.parseInt;
 
+@SuppressWarnings("ALL")
 public class art2 {
 
-    private static class Range {
-        int start, end, color;
+    private static class Node {
+        int location, color;
+        boolean single = false;
 
-        public Range(int start, int color) {
-            this.start = start;
-            this.end = -1;
+        public Node(int location, int color) {
+            this.location = -1;
             this.color = color;
         }
     }
@@ -25,31 +23,53 @@ public class art2 {
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("art2.out")));
 
         int paintingSize = parseInt(in.readLine());
-        Range[] ranges = new Range[paintingSize];
+        Node[] nodes = new Node[paintingSize];
         for (int i = 0; i < paintingSize; i++) {
             int color = parseInt(in.readLine());
             if (color != 0) {
-                if (ranges[color - 1] == null) {
-                    ranges[color - 1] = new Range(i, color);
+                if (nodes[color - 1] == null) {
+                    nodes[color - 1] = new Node(i, color);
                 } else {
-                    ranges[color - 1].end = i;
+                    nodes[color - 1].single = true;
                 }
             }
         }
 
-        Arrays.sort(ranges, (range, t1) -> {
-            if (range == null) {
+        Arrays.sort(nodes, (node, t1) -> {
+            if (node == null && t1 == null) {
+                return 0;
+            } else if (node == null) {
                 return 1;
             } else if (t1 == null) {
                 return -1;
             }
-            return range.start - t1.start;
+            return node.location - t1.location;
         });
 
         Stack<Integer> paint = new Stack<>();
-//        int maxSize = 0;
-//        for (int i = 0; i < ; i++) {
-//
-//        }
+        int maxSize = 0;
+        boolean[] added = new boolean[paintingSize];
+        for (int i = 0; i < paintingSize; i++) {
+            if (nodes[i] == null) {
+                break;
+            }
+
+            int color = nodes[i].color;
+            if (added[color - 1]) {
+                paint.pop();
+            } else {
+                if (nodes[i].single) {
+                    added[color - 1] = true;
+                    maxSize = max(paint.size() + 1, maxSize);
+                } else {
+                    added[color - 1] = true;
+                    maxSize = max(paint.size() + 1, maxSize);
+                    paint.add(color);
+                }
+            }
+        }
+
+        out.println(maxSize);
+        out.close();
     }
 }
